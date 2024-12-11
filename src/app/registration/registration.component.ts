b86ef40aca1f00393fe95bdfbb8966773c01bdb1
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2024-12-11 23:25:28 +0530

    modified

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,17 +7,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-  formgroup!:FormGroup;
-  constructor(private fb:FormBuilder){
-    this.formgroup=this.fb.group([
-      {
-        username:['',[Validators.required,Validators.minLength(3)]],
-        email:['',[Validators.required,Validators.email,Validators.pattern('')]],
-        password:['',[Validators.required,Validators.minLength(8),Validators.maxLength(15)]],
-        phonenumber:['',Validators.required,Validators.minLength(10),Validators.maxLength(11)]
+  formgroup!: FormGroup;
+  constructor(private fb: FormBuilder) {
+      this.formgroup = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
+      phonenumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]]
+    });
+  }
 
-      }
-    ])
-   }
+  onsubmit() {
+    if (this.formgroup.valid) {
+      alert('successfully inserted')
+    }
+    else {
+      alert('error')
+    }
 
+  }
 }
--- src/app/spotifydashboard/spotifydashboard.component.ts
+++ src/app/spotifydashboard/spotifydashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SpotifyService } from '../services/spotify.service';
 import { Spotify } from '../models/spotify';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-spotifydashboard',
@@ -8,13 +9,24 @@ import { Spotify } from '../models/spotify';
   styleUrls: ['./spotifydashboard.component.css']
 })
 export class SpotifydashboardComponent {
-  records:Spotify[]=[]
-  constructor(private service:SpotifyService){}
+  records:Spotify[]=[];
+  formgroup!:FormGroup;
+  constructor(private service:SpotifyService,private fb:FormBuilder){
+    this.formgroup=this.fb.group({
+      username:['',[Validators.required]]
+
+    })
+  }
 
   ngOnInit():void{
     this.getrecords();
   }
   getrecords():void{
     this.service.getall().subscribe(data=>this.records=data)
+  }
+
+  updaterecords(){
+    
+
   }
 }
